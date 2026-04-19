94c1a57db863d8f509289b5b681885c3c48cba54
tridipdowari <tridipdowari75@gmail.com>
2026-04-19 19:27:20 +0530

    added AboutUs page

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,10 @@
+export default function AboutUs() {
+  return (
+    <div className="container">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a plant shop offering a variety of indoor and outdoor plants.
+      </p>
+    </div>
+  );
+}
